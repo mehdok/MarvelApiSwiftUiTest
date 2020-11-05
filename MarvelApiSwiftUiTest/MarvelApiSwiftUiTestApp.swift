74a29a714f3727cdcc25b5d2daf7af9aa4f204e0
Mehdi Sohrabi <mehdok@gmail.com>
2020-11-05 12:47:58 +0330

    Initial Commit

---
+++ MarvelApiSwiftUiTest/MarvelApiSwiftUiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApiSwiftUiTestApp.swift
+//  MarvelApiSwiftUiTest
+//
+//  Created by Mehdok on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApiSwiftUiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
